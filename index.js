8fdaeaacefa7a251fe5fb2bb68fb3ba542ca9c44
Wright <calebogundiya@gmail.com>
2020-06-03 22:26:16 +0100

    Setup for creating private blockchain

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let express = require('express');
 
 let app = express();
 
-app.get('/', (req, res)=>{
+app.get('/block/:id', (req, res)=>{
     res.status(200).json({
         "body" : "Loaded index page"
     })
 });
 
-app.post('/', (req, res)=>{});
+app.post('/', (req, res, next)=>{
+    console.log(req.status);
+    next()
+});
 
 app.listen('8080', console.log("Listening on port 8080"));
