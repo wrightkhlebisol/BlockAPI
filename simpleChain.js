ff9fae3c44512461a0d4ce846ff1e0576432a743
Wright <calebogundiya@gmail.com>
2020-06-04 00:05:46 +0100

    Implemented timestamp for block creation time and implemented block height function

--- simpleChain.js
+++ simpleChain.js
@@ -21,13 +21,19 @@ class Block {
 class BlockChain {
     constructor() {
         this.chain = [];
+        // Create the genesis block
         this.addBlock(new Block("First block in the chain - Genesis Block"));
     }
 
     addBlock(newBlock) {
         if (this.chain.length > 0) {
+            // Assign previous blocks hash for subsequent blocks from Genesis
             newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash
         }
+        newBlock.height = this.chain.length;
+        // Include timestamp following the UTC format by slicing the stringed timestamp
+        newBlock.time = new Date().getTime().toString().slice(0, -3);
+        // SHA256 Requires a string of data
         newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
         this.chain.push(newBlock);
     }
